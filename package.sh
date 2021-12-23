7c68c3ea45df3f66ba45dd87616bee20af26ee88
Anton Jansson <anton_jansson@live.se>
2021-12-23 12:46:51 +0100

    No need to create zip as GitHub actions zips artifacts anyway.

--- package.sh
+++ package.sh
@@ -3,5 +3,3 @@ mkdir -p imageeditor
 cp target/release/imageeditor imageeditor/imageeditor_bin
 cp run_imageeditor.sh imageeditor/imageeditor
 cp -r content imageeditor/content
-zip -r imageeditor.zip imageeditor/*
-rm -rf imageeditor
